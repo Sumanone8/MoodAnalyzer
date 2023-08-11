88fec99fdc8baf5ab6196952f9047d3d070a2838
Sumanone8 <sumanjhabhl@gmail.com>
2023-08-11 09:36:58 +0530

    Suman : Implement MoodAnalyser class for mood analysis
    - Added MoodAnalyser class with AnalyseMood method to determine mood based on certain criteria.
    - Handled null or empty message input by providing a default empty string.
    - Implemented basic user interaction to input a message and display the analyzed mood result.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class MoodAnalyser
+{
+    public string AnalyseMood(string message)
+    {
+        // Check for null or empty message
+        if (string.IsNullOrWhiteSpace(message))
+        {
+            return "Neutral Mood";
+        }
+
+        // Perform mood analysis based on certain criteria
+        if (message.ToLower().Contains("happy") || message.ToLower().Contains("joy"))
+        {
+            return "Happy Mood";
+        }
+        else if (message.ToLower().Contains("sad") || message.ToLower().Contains("unhappy"))
+        {
+            return "Sad Mood";
+        }
+        else
+        {
+            return "Neutral Mood";
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create an instance of MoodAnalyser
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+
+        // Get input message from the user
+        Console.Write("Enter a message: ");
+        string message = Console.ReadLine();
+
+        // Analyze the mood and display the result
+        string mood = moodAnalyser.AnalyseMood(message ?? ""); // Handle null by providing a default empty string
+        Console.WriteLine("Mood: " + mood);
+    }
+}
